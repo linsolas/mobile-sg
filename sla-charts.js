82d62f7546ec0c94df1d8d096ada5f8798eff1b5
Romain Linsolas <linsolas@gmail.com>
2013-03-14 16:49:47 +0100

    Create sla-charts.js

---
+++ sla-charts.js
@@ -0,0 +1,72 @@
+function initPieChart() {
+    var data = [
+        ['Closed Deals', 12],
+        ['Deals in progress', 9],
+        ['Deals with warnings', 3],
+        ['Deals with errors', 2],
+        ['Deals not started', 7]
+    ];
+
+    var plot1 = jQuery.jqplot('chart1', [data],
+        {
+            seriesDefaults: {
+                // Make this a pie chart.
+                renderer: jQuery.jqplot.PieRenderer,
+                rendererOptions: {
+                    // Put data labels on the pie slices.
+                    // By default, labels show the percentage of the slice.
+                    showDataLabels: true
+                }
+            },
+            legend: { show: true, location: 'e' }
+        }
+    );
+}
+
+
+function initBarChart() {
+    var s1 = [200, 600, 700, 1000];
+    var s2 = [460, -210, 690, 820];
+    var s3 = [-260, -440, 320, 200];
+// Can specify a custom tick Array.
+// Ticks should match up one for each y value (category) in the series.
+    var ticks = ['May', 'June', 'July', 'August'];
+
+    var plot1 = $.jqplot('chart2', [s1, s2, s3], {
+        // The "seriesDefaults" option is an options object that will
+        // be applied to all series in the chart.
+        seriesDefaults: {
+            renderer: $.jqplot.BarRenderer,
+            rendererOptions: {fillToZero: true}
+        },
+        // Custom labels for the series are specified with the "label"
+        // option on the series option.  Here a series option object
+        // is specified for each series.
+        series: [
+            {label: 'Hotel'},
+            {label: 'Event Regristration'},
+            {label: 'Airfare'}
+        ],
+        // Show the legend and put it outside the grid, but inside the
+        // plot container, shrinking the grid to accomodate the legend.
+        // A value of "outside" would not shrink the grid and allow
+        // the legend to overflow the container.
+        legend: {
+            show: true,
+            placement: 'outsideGrid'
+        },
+        axes: {
+            // Use a category axis on the x axis and use our custom ticks.
+            xaxis: {
+                renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: ticks
+            },
+            // Pad the y axis just a little so bars can get close to, but
+            // not touch, the grid boundaries.  1.2 is the default padding.
+            yaxis: {
+                pad: 1.05,
+                tickOptions: {formatString: '$%d'}
+            }
+        }
+    });
+}
